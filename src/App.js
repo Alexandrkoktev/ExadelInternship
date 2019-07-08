f27f92763b79df64de101a780cd5e73c700a0a97
Dmitriy <dmitriy_metelskiy@mail.ru>
2019-07-08 13:36:40 +0300

    add Greetings

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Greetings from './Greetings';
 
 function App() {
+  const name = '11111';
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Greetings name = {name}/>
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/Greetings.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Greetings extends React.Component {
+    render() {
+        const {name} = this.props;
+
+        return (
+            <div>
+                Hello, {name}
+            </div>
+            )
+    }
+}
+
+export default Greetings;
