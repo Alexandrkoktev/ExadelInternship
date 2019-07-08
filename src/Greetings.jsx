af43a5652000f917decd95df5207cfc33dd33089
NadezhdaSukhotskaya <47493685+NadezhdaSukhotskaya@users.noreply.github.com>
2019-07-08 18:21:51 +0300

    fixed bugs

--- src/Greetings.jsx
+++ src/Greetings.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 
 class Greetings extends React.Component {
+
     render() {
-        const {name} = this.props;
+        // eslint-disable-next-line react/prop-types
+        const { name } = this.props;
 
         return (
             <div>
                 Hello, {name}
             </div>
-            )
+        )
     }
+
 }
 
 export default Greetings;
