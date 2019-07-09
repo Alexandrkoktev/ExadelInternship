7557e2be0ae72a671fe8e13875ad2e9454303a4c
Ilya <lyah.ilya88@gmail.com>
2019-07-09 12:27:32 +0300

    Eslint+Prettier

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Greetings from './Greetings';
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+// eslint-disable-next-line no-unused-vars
+import Greetings from './Greetings'
 
 function App() {
-  const name = '11111';
+  const name = '11111'
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Greetings name = {name}/>
+        <Greetings name={name} />
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -20,7 +22,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
