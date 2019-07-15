86ca0f5104edfaf855de02df0db0011d56414c28
polza <polza.helpme@yahoo.com>
2019-07-15 13:06:15 +0300

    New func for list in routes

--- src/components/list-components/CustomList.jsx
+++ src/components/list-components/CustomList.jsx
@@ -3,20 +3,29 @@ import { ListGroup, Tabs, Tab } from 'react-bootstrap'
 import CustomListItem from './CustomListItem'
 
 class CustomList extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      size: props.size,
+    }
+  }
+
+  createList = (num, text) => {
+    let list = []
+    for (let i = 0; i < num; i++) {
+      list.push(<CustomListItem my_text={text} routeid={i} />)
+    }
+    return list
+  }
+
   render() {
     return (
       <Tabs>
         <Tab eventKey="passenger" title="Passenger">
-          <ListGroup>
-            <CustomListItem my_text="Passenger" routeid="1" />
-            <CustomListItem my_text="Passenger" routeid="2" />
-          </ListGroup>
+          <ListGroup>{this.createList(3, 'Passenger')}</ListGroup>
         </Tab>
         <Tab eventKey="driver" title="Driver">
-          <ListGroup>
-            <CustomListItem my_text="Driver" routeid="3" />
-            <CustomListItem my_text="Driver" routeid="4" />
-          </ListGroup>
+          <ListGroup>{this.createList(20, 'Driver')}</ListGroup>
         </Tab>
       </Tabs>
     )
