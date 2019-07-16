64c4d256127d02a782655cf869c73d5b1cd92b26
Ilya <lyah.ilya88@gmail.com>
2019-07-16 17:50:05 +0300

    fixes+(pretty footer)

--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -8,9 +8,9 @@ import Routes from '../../containers/routes/Routes'
 import Profile from '../../containers/profile/Profile'
 // eslint-disable-next-line no-unused-vars
 import Home from '../../containers/home/Home'
-import NewRoute from '../../containers/newRoute/NewRoute'
+import NewRoute from '../../containers/new-route/NewRoute'
 import OneRouteInfo from '../../containers/one-route/OneRouteInfo'
-import NewRide from '../../containers/newRide/NewRide'
+import NewRide from '../../containers/new-ride/NewRide'
 
 class Content extends React.Component {
   render() {
@@ -18,15 +18,15 @@ class Content extends React.Component {
       <Switch>
         <Route exact path="/" component={LogIn} />
         <Route exact path="/home" component={Home} />
+        <Route exact path="/home/new-ride" component={NewRide}/>
+        <Route exact path="/home/new-route" component={NewRoute}/>
         <Route exact path="/routes" component={Routes} />
         <Route
           exact
           path="/routes/route-info/:routeid"
           component={OneRouteInfo}
         />
-
         <Route exact path="/profile" component={Profile} />
-        <Route exact path="/new-ride" component={NewRide}/>
         <Route render={() => <h1>Page not found</h1>} />
       </Switch>
     )
