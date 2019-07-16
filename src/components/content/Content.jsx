7bcc8f8f9b536b416943e31aed5f498f5cd9f88a
polza <polza.helpme@yahoo.com>
2019-07-16 16:09:45 +0300

    Some merw fixes

--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -3,14 +3,17 @@ import LogIn from '../../containers/login/LogIn'
 // eslint-disable-next-line no-unused-vars
 import { Switch, Route } from 'react-router-dom'
 import Routes from '../../containers/routes/Routes'
-import OneRouteInfo from '../../containers/routeinfo/OneRouteInfo'
+import OneRouteInfo from '../../containers/one-route/OneRouteInfo'
 import NewRoute from '../../containers/newRoute/NewRoute'
+import Home from '../../containers/Home'
+import Profile from '../../containers/profile/Profile'
 
 class Content extends React.Component {
   render() {
+    var flag = false
     return (
       <Switch>
-        <Route exact path="/" component={LogIn} />
+        <Route exact path="/" component={flag ? Home : LogIn} />
         <Route exact path="/routes" component={Routes} />
         <Route
           exact
@@ -18,6 +21,7 @@ class Content extends React.Component {
           component={OneRouteInfo}
         />
         <Route exact path="/new-ride" component={NewRoute} />
+        <Route exact path="/profile" component={Profile} />
         <Route render={() => <h1>Page not found</h1>} />
       </Switch>
     )
