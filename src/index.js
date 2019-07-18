ab09998790c3e7a9056315d8736cbe0aa27f0b13
Ilya <lyah.ilya88@gmail.com>
2019-07-18 17:34:40 +0300

    zachem ja eto delal?

--- src/index.js
+++ src/index.js
@@ -5,13 +5,18 @@ import './index.css'
 // eslint-disable-next-line no-unused-vars
 import App from './App'
 import * as serviceWorker from './serviceWorker'
-import store from './store/store'
 // eslint-disable-next-line no-unused-vars
 import { Provider } from 'react-redux'
-
+// eslint-disable-next-line no-unused-vars
+import { ConnectedRouter } from 'connected-react-router'
+// eslint-disable-next-line no-unused-vars
+import configureStore, { history } from './store/store'
+const store = configureStore()
 ReactDOM.render(
   <Provider store={store}>
+    <ConnectedRouter history={history}>
     <App />
+    </ConnectedRouter>
   </Provider>,
   document.getElementById('root')
 )
