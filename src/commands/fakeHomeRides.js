8825da78fb976cd89755c9eaf90678b3759010e8
Dmitriy <dmitriy_metelskiy@mail.ru>
2019-07-19 19:50:14 +0300

    finished rendering homepage using Redux

--- src/commands/fakeHomeRides.js
+++ src/commands/fakeHomeRides.js
@@ -4,36 +4,42 @@ export const fakeHomeRides = () => {
     return delay({
     passengerRides: [
         {
-        id: 1,
-        depPoint: 'depPoint1',
-        destPoint: 'destPoint1',
-        depTime: 'depTime1'
+            id: 1,
+            depPoint: 'ул. Якуба Коласа, 5',
+            destPoint: 'ул. Академика Купревича, 3',
+            depTime: '25.07.2019 10:30'
         },
         {
-        id: 2,
-        depPoint: 'depPoint2',
-        destPoint: 'destPoint2',
-        depTime: 'depTime2'
+            id: 2,
+            depPoint: 'ул. Академика Купревича, 3',
+            destPoint: 'Каменная Горка',
+            depTime: '25.07.2019 18:30'
         },
         {
-        id: 3,
-        depPoint: 'depPoint3',
-        destPoint: 'destPoint3',
-        depTime: 'depTime3'
+            id: 3,
+            depPoint: 'ул. Академика Купревича, 3',
+            destPoint: 'Каменная Горка',
+            depTime: '26.07.2019 18:30'
         }
     ],
     driverRides: [
         {
-        id: 4,
-        depPoint: 'depPoint1',
-        destPoint: 'destPoint1',
-        depTime: 'depTime1'
+            id: 4,
+            depPoint: 'Каменная Горка',
+            destPoint: 'ул. Академика Купревича, 3',
+            depTime: '22.06.2019 10:00'
         },
         {
-        id: 5,
-        depPoint: 'depPoint2',
-        destPoint: 'destPoint2',
-        depTime: 'depTime2'
+            id: 5,
+            depPoint: 'Каменная Горка',
+            destPoint: 'ул. Академика Купревича, 3',
+            depTime: '23.06.2019 10:00'
+        },
+        {
+            id: 6,
+            depPoint: 'Каменная Горка',
+            destPoint: 'ул. Академика Купревича, 3',
+            depTime: '24.06.2019 10:00'
         }
     ]
 })
--- src/containers/home/Home.jsx
+++ src/containers/home/Home.jsx
@@ -7,51 +7,13 @@ import { connect } from 'react-redux'
 import { mapStateToProps, mapDispatchToProps } from '../../commands/home'
 
 class Home extends React.Component {
-/*
-  requestRides = () => {
-    this.passengerRides = [
-      {
-        id: 1,
-        depPoint: 'depPoint1',
-        destPoint: 'destPoint1',
-        depTime: 'depTime1'
-      },
-      {
-        id: 2,
-        depPoint: 'depPoint2',
-        destPoint: 'destPoint2',
-        depTime: 'depTime2'
-      },
-      {
-        id: 3,
-        depPoint: 'depPoint3',
-        destPoint: 'destPoint3',
-        depTime: 'depTime3'
-      }
-    ];
-    this.driverRides = [
-      {
-        id: 4,
-        depPoint: 'depPoint1',
-        destPoint: 'destPoint1',
-        depTime: 'depTime1'
-      },
-      {
-        id: 5,
-        depPoint: 'depPoint2',
-        destPoint: 'destPoint2',
-        depTime: 'depTime2'
-      }
-    ]
-  }
-*/
   componentDidMount() {
     this.props.requestRides();
   }
 
   render() {
     const { homeRides: {passengerRides, driverRides} } = this.props;
-    alert(passengerRides);
+
     return (
       <>
         <AddRouteBTNs />
