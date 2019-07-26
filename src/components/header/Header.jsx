c4f23955a01c1941ad02201d784f586a725da530
NadezhdaSukhotskaya <47493685+NadezhdaSukhotskaya@users.noreply.github.com>
2019-07-26 21:53:16 +0300

    delete link to home for guest

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 // eslint-disable-next-line no-unused-vars
 import { Nav, Navbar } from 'react-bootstrap'
 // eslint-disable-next-line no-unused-vars
-import { Link } from 'react-router-dom'
+import { Link, Redirect } from 'react-router-dom'
 // eslint-disable-next-line no-unused-vars
 import NavDropdown from 'react-bootstrap/NavDropdown'
 import { connect } from 'react-redux'
@@ -26,11 +26,13 @@ class Header extends React.Component {
           variant="dark"
           sticky="top"
         >
-          <Link to="/home">
+          {!isLoggedIn && <>
             <Navbar.Brand>CarPool</Navbar.Brand>
-          </Link>
+          </>}
           {isLoggedIn && (
             <>
+              <Link to="/home"> <Navbar.Brand>CarPool</Navbar.Brand>
+              </Link>
               <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
               <Navbar.Collapse id="responsive-navbar-nav">
                 <Nav className="mr-auto"/>
